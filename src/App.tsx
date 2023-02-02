c44d2807adebc1973d705c9564edb754b7c75eec
Haoran Weng <weng1043282095@gmail.com>
2023-02-01 20:28:43 -0500

    importing with shpjs and react-leaflet

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,42 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import { MapContainer, TileLayer, GeoJSON  } from 'react-leaflet';
+import shp from "shpjs";
 
 function App() {
+  
   const [count, setCount] = useState(0)
+  const [geojson, setGeojson] = useState(null)
+  const position = [51.505, -0.09]
+
+  const uploadHandler = (file: File) => {
+    const url = window.URL.createObjectURL(file);
+    shp(url).then(function(geojson){
+      setGeojson(geojson)
+    });
+  }
+
+  var Geojson = null;
+  if(geojson){
+    Geojson = <GeoJSON data={geojson} />
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <div>
+    <input type="file" 
+        id="myFile" 
+        name="filename"  
+        onChange={(e)=>uploadHandler(e.target.files![0])}
+      />
+    <MapContainer center={position} zoom={3}>
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    {Geojson}
+  </MapContainer>
+  </div>
   )
 }
 
